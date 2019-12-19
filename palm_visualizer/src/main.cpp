650440c943bb7b09dd283c1acf2c1aeb3f288516
ramkalath <ramkalath@gmail.com>
2019-12-19 22:06:18 +0530

    VR palm visualizer

--- palm_visualizer/src/main.cpp
+++ palm_visualizer/src/main.cpp
@@ -2,8 +2,10 @@
  * Author : Ram
  * Date : 19/Dec/2019
  * Email : ramkalath@gmail.com
- * Breif Description : icosphere rendering
- * Detailed Description : icosphere loading with assimp
+ * Breif Description : handvisualizer with leapmotion
+ * Detailed Description : working fine as a VR visualizer
+ *						 TODO(ram): yet to make it AR; lots of work left for that				
+ * 						 TODO(ram): draw line primitives between the finger joints (not too critical)
  *****************************************************************************/
 // GLEW and GLFW includes
 #define GLEW_STATIC
@@ -24,6 +26,9 @@
 #include "../include/modelloader.h"
 #include "../include/gamesettings.h"
 
+// Leap motion includes
+#include "../include/finger_joints.h"
+
 void key_callback(GLFWwindow *window, int key, int scancode, int action, int mode)
 {
 	// When the user presses the escape key, we set the window should close property to true, and close the application.
@@ -32,9 +37,11 @@ void key_callback(GLFWwindow *window, int key, int scancode, int action, int mod
 }
 
 int width = 640, height = 800;
+int spread = 150;
 
 int main()
 {
+	Hands hand;
 	GameSettings globalsettings;
 	// glfw stuff ====================================================================
     glfwInit();
@@ -66,13 +73,6 @@ int main()
 	Shader objectshader("./shaders/vertex_shader.vert", "./shaders/fragment_shader.frag");
 
 	modelloader icosphere("./resources/icosphere/icosphere.obj");
-	icosphere.modelmatrix = {1.0f, 0.0f, 0.0f, 0.0f,
-							 0.0f, 1.0f, 0.0f, 0.0f, 
-							 0.0f, 0.0f, 1.0f, 0.0f, 
-							 0.0f, 0.0f, 0.0f, 1.0f};
-	//icosphere.modelmatrix = glm::translate(icosphere.modelmatrix, glm::vec3(0.0f, 0.0f, 0.0f));
-	//icosphere.modelmatrix = glm::rotate(icosphere.modelmatrix, glm::radians(45.0f), glm::vec3(1.0f, 1.0f, 0.0f));
-	icosphere.modelmatrix = glm::scale(icosphere.modelmatrix, glm::vec3(0.1f, 0.1f, 0.1f));
 
 
 	while(!glfwWindowShouldClose(window))
@@ -93,14 +93,26 @@ int main()
 		glUniform3f(glGetUniformLocation(objectshader.program, "LightSpecular"), 1.0f, 1.0f, 1.0f);
 		glUniform3f(glGetUniformLocation(objectshader.program, "CameraPosition"), 0.0f, 0.0f, 0.0f);
 		
-		// render icosphere
-		glUniformMatrix4fv(glGetUniformLocation(objectshader.program, "model"), 1, GL_FALSE, glm::value_ptr(icosphere.modelmatrix));
 
-		auto start = std::chrono::high_resolution_clock::now();
-		icosphere.RenderModel();
-		auto stop = std::chrono::high_resolution_clock::now();
-		auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop-start);
-		std::cout << "time taken for rendering = " << duration.count() << " microseconds" << std::endl;
+		if(hand.detect_hands())
+		{
+			for(unsigned long i=0; i<hand.vertices_hand.size(); i++)
+			{
+				// Print joint positions
+				//std::cout << "joint " << i << "\t"
+						  //<< hand.vertices_hand[i].x/200 << "\t"
+						  //<< hand.vertices_hand[i].y/200 << "\t"
+						  //<< hand.vertices_hand[i].z/200 << std::endl;
+
+				icosphere.modelmatrix = {0.03f, 0.0f, 0.0f, 0.0f,
+										 0.0f, 0.03f, 0.0f, 0.0f, 
+										 0.0f, 0.0f, 0.03f, 0.0f, 
+										 hand.vertices_hand[i].x/spread, hand.vertices_hand[i].y/spread, hand.vertices_hand[i].z/spread, 1.0f};
+						  
+				glUniformMatrix4fv(glGetUniformLocation(objectshader.program, "model"), 1, GL_FALSE, glm::value_ptr(icosphere.modelmatrix));
+				icosphere.RenderModel();
+			}
+		}
 
 		glfwSwapBuffers(window);
 	}
