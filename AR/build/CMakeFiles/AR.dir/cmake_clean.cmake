7715b09bf9e8c9787ee4211f9920479f167ff5e3
ramkalath <ramkalath@gmail.com>
2020-01-08 09:18:47 +0530

    dummy commit

---
+++ AR/build/CMakeFiles/AR.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../bin/AR"
+  "../bin/AR.pdb"
+  "CMakeFiles/AR.dir/src/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AR.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
